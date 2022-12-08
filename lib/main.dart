1c2cdc5de905b86578c32ac492d7f2e51750529c
hanatani <adgjmptwhanatani@gmail.com>
2022-12-09 00:34:28 +0900

    フォーマッターの反映

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,85 +33,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('色を選ぶ'),
+        title: Text('色選ぶ'),
       ),
       body: Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-         children:[
-
-           ElevatedButton(
-          child: Text(
-              '青',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-          ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.blue,
-            fixedSize: Size(100, 100),
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => BluePage()),
-            );
-          },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text(
+                '青',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                fixedSize: Size(100, 100),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BluePage()),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text(
+                '赤',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                fixedSize: Size(100, 100),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RedPage()),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('黄',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  )),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow,
+                fixedSize: Size(100, 100),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => YellowPage()),
+                );
+              },
+            ),
+          ],
         ),
-
-        ElevatedButton(
-          child: Text(
-              '赤',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-          ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.red,
-            fixedSize: Size(100, 100),
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => RedPage()),
-            );
-          },
-        ),
-
-           ElevatedButton(
-             child: Text(
-                 '黄',
-             style: TextStyle(
-               fontSize: 30,
-               color: Colors.black,
-               fontWeight: FontWeight.bold,
-             )
-             ),
-             style: ElevatedButton.styleFrom(
-               backgroundColor: Colors.yellow,
-               fixedSize: Size(100, 100),
-             ),
-             onPressed: () {
-               Navigator.push(
-                 context,
-                 MaterialPageRoute(builder: (context) => YellowPage()),
-               );
-             },
-           ),
-    ],
-
       ),
-    ),
     );
   }
 }
